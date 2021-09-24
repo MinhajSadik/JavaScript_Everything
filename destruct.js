d169e0a017947fd489b35d8204606a9d84906cd5
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-24 21:45:16 +0600

    [add] destructuring new way

--- destruct.js
+++ destruct.js
@@ -23,3 +23,23 @@ const myObject = {
 const {liberary: {frameworkA: {liberaryR} = {}}} = myObject;
 
 console.log(liberaryR);
+
+const myArray = [1, 12, 13, [14, 15, 16], 17, 18]
+
+// const [num, num1, num2] = myArray;
+
+// console.log(num, num1, num2);
+
+const [,,num13, [num14],,,num17] = myArray;
+
+// num 17' is undefiend
+console.log(num13, num14, num17); 
+
+
+var a = 1;
+
+var b = 2;
+
+[b, a] = [a, b];
+
+console.log(a, b);
