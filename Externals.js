c398d0221af9a3f09def50995a3b1d5a40947f6b
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-24 22:11:35 +0600

    [add] module import export in javascript

---
+++ Externals.js
@@ -0,0 +1,7 @@
+export const numbers = [1, 2, 3, 4]
+const obj = {a: 1, b: 2, c: 3}
+// export default obj
+
+export default function myFunc() {
+    console.log("i am from myFunc()");
+}
---
+++ IEmodule.js
@@ -0,0 +1,16 @@
+// import { numbers, obj } from './external.js'; //named import
+
+// import * as test from './external.js'; // alieas way
+
+// import { numbers as num, obj as object } from './external.js'; // named import
+
+// import Externals, { numbers } from './Externals.js'; export default and named export
+
+// console.log(Externals, numbers);
+
+// import { myFunc } from './Externals.js'; // function import
+
+// import myFuncDefault from './Externals.js'; // function import default
+
+// myFuncDefault()
+// myFunc()
