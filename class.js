84cb73b957ca3f48e0affb929b782d510bd85478
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-25 12:34:18 +0600

    [add] class and oop syntex recap in javascript

--- class.js
+++ class.js
@@ -1,3 +1,23 @@
+// class Person {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     eat(){
+//         console.log(`${this.name} is eating`);
+//     }
+//     sleep(){
+//         console.log(`${this.name} is sleeping`);
+//     }
+//     play(){
+//         console.log(`${this.name} is playing`);
+//     }
+// }
+
+// const sakib = new Person(`Sakib`, 43)
+// sakib.eat();
+
+
 class Person {
     constructor(name, age){
         this.name = name;
@@ -6,13 +26,22 @@ class Person {
     eat(){
         console.log(`${this.name} is eating`);
     }
-    sleep(){
-        console.log(`${this.name} is sleeping`);
+}
+
+class Cricketer extends Person {
+    constructor(name, age, type, country){
+        super(name, age);
+
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.country = country;
     }
-    play(){
-        console.log(`${this.name} is playing`);
+    paly(){
+        console.log(`${this.name} is palying`);
     }
 }
 
-const sakib = new Person(`Sakib`, 43)
-sakib.eat();
+const sakib = new Cricketer('sakib', 43, 'all rounder', 'bangladesh')
+
+sakib.paly();
