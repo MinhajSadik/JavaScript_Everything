d2acb58f9930e0520d8ec94799e04883aea67ef6
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-26 02:47:50 +0600

    [add] example 1 without higher order function and with hof code example

--- function.js
+++ function.js
@@ -6,3 +6,21 @@ function hello() {
 }
 
 hello()();
+
+
+// example 1 without higher order fuction 
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+var result = [];
+
+for (let i = 0; i < numbers.length; i++) {
+result.push(numbers[i])    
+}
+
+console.log(result);
+
+
+// example 1 with higher order fuction 
+var result = numbers.map((number) => number * 2)
+
+console.log(result);
