196d3c2e5f967e7b782af1844d5022003f1696f9
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-26 02:49:47 +0600

    [add] example 2 without higher order function and with hof code example

--- function.js
+++ function.js
@@ -24,3 +24,40 @@ console.log(result);
 var result = numbers.map((number) => number * 2)
 
 console.log(result);
+
+// example 2 without higher order fuction 
+var players = [
+    {
+        name: 'sakib',
+        avg: 30.0,
+    },
+    {
+        name: 'tamim',
+        avg: 34.0,
+    },
+    {
+        name: 'mahmudullah',
+        avg: 100.00,
+    },
+    {
+        name: 'musfiq',
+        avg: 49.23,
+    },
+]
+
+
+var playerAvgWithFourty = [];
+
+for (let i = 0; i < players.length; i++) {
+    if(players[i].avg >= 40){
+        playerAvgWithFourty.push(players[i])
+    }  
+}
+
+console.log(playerAvgWithFourty);
+
+// example 2 with higher-order-fuction 
+var playersWithAvgFourty = players.filter((player) => player.avg >= 40)
+
+console.log(playersWithAvgFourty);
+
