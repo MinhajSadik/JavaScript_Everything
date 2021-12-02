d9c996c8972de8cd5d9247aafb7b2e10b8b2918d
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-02 20:02:29 +0600

    [ADD] find array max value in local array

---
+++ JavaScripts/JS_Problems/Array.max.js
@@ -0,0 +1,10 @@
+const arr = [99, 32, 23, 45];
+const max = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  if (element > max) {
+    max = element;
+  }
+}
+
+console.log(max);
