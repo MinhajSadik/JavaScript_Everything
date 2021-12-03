190db6fe42de321bbf6cd06c633e2866fdbf3a5d
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-03 19:36:05 +0600

    [ADD] javascript coding problem [solved]

--- JavaScripts/JS_Problems/Try_Problems.js
+++ JavaScripts/JS_Problems/Try_Problems.js
@@ -1,21 +1,21 @@
 var a, b, c;
 // 2. What will be the output of following code ?
-// console.log(typeof isNaN(+"6"));
+console.log(typeof isNaN(+"6"));
 
-// console.log(typeof +String("true"));
+console.log(typeof +String("true"));
 
 // 7. What will be the output of following code ?
-// console.log("Hello" + "Javascript" + "Lover" + +-55);
-// console.log(true + !!"false");
-// console.log(5 * "5" + "10" - "2000");
+console.log("Hello" + "Javascript" + "Lover" + +-55);
+console.log(true + !!"false");
+console.log(5 * "5" + "10" - "2000");
 
 // 8. What will be output of following code ?
-// var a = 5;
-// var b = ++a;
-// b = b++;
-// var c = b;
-// console.log(b);
-// console.log(c);
+var a = 5;
+var b = ++a;
+b = b++;
+var c = b;
+console.log(b);
+console.log(c);
 
 // 9. What will be output of following code?
 a = null;
