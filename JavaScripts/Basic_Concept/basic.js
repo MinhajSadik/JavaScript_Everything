da5ecd8498944e2fff61750935ff1eab73b04ace
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-07 19:05:25 +0600

    [Change] naming canvension

--- JavaScripts/Basic_Concept/basic.js
+++ JavaScripts/Basic_Concept/basic.js
@@ -21,7 +21,7 @@
 const array = [1, 2, 3, 4, 5];
 const friends_age = [5, 6, 7, 8, 9, 10];
 
-function evenify_condition(givenValue) {
+function evenifyCondition(givenValue) {
   if (givenValue % 2 === 0) {
     return givenValue;
   } else if (givenValue % 2 !== 0) {
@@ -33,8 +33,8 @@ export function shortFunctionCall(givenValue) {
   let evenify_array = [];
   for (let i = 0; i < givenValue.length; i++) {
     const givenNum = givenValue[i];
-    const evenifyCondition = evenify_condition(givenNum);
-    evenify_array.push(evenifyCondition);
+    const resultEvenifyCondition = evenifyCondition(givenNum);
+    evenify_array.push(resultEvenifyCondition);
   }
   return evenify_array;
 }
