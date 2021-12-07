a34e012e0fe5a0a1cd9ea9f801ab66f17ef1c2a7
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-07 18:02:17 +0600

    [Add] javascript loop condition evenify & it can batter performe

--- JavaScripts/Basic_Concept/basic.js
+++ JavaScripts/Basic_Concept/basic.js
@@ -21,16 +21,25 @@
 const array = [1, 2, 3, 4, 5];
 const friends_age = [5, 6, 7, 8, 9, 10];
 
-export function shortFunctionCall(givenValue) {
-  for (let i = 0; i < givenValue.length; i++) {
-    const givenNum = givenValue[i];
-    if (givenNum % 2 === 0) {
-      console.log(`Jur Sonkha: ${givenNum}`);
-    } else if (givenNum % 2 !== 0) {
-      console.log(` Jor Sonkha Hoye Gese: ${givenNum * 2}`);
-    }
+function evenify_condition(givenValue) {
+  if (givenValue % 2 === 0) {
+    return givenValue;
+  } else if (givenValue % 2 !== 0) {
+    return givenValue * 2;
   }
 }
 
-shortFunctionCall(friends_age);
-shortFunctionCall(array);
+export function shortFunctionCall(givenValue) {
+  let evenify_array = [];
+  for (let i = 0; i < givenValue.length; i++) {
+    const givenNum = givenValue[i];
+    const evenifyCondition = evenify_condition(givenNum);
+    evenify_array.push(evenifyCondition);
+  }
+  return evenify_array;
+}
+
+const resultFriends_age = shortFunctionCall(friends_age);
+console.log(resultFriends_age);
+const resultArray = shortFunctionCall(array);
+console.log(resultArray);
