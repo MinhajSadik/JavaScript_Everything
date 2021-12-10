e1b6da1b2d61ef2ac45960b049b6b22756717adb
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-10 07:18:30 +0600

    [ADD] nodemon node_modules and playground.js file added

---
+++ playground.js
@@ -0,0 +1,12 @@
+function addition(a, b) {
+  return a + b;
+}
+const resultAddition = addition(1, 2);
+console.log(resultAddition);
+
+const bioData = {
+  name: "Minhaj Sadik",
+  age: 21,
+  address: "123 Main St",
+};
+console.log(bioData.name);
