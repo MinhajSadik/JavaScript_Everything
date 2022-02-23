7166397a8be7d1e1bc3c9a3648c224ee40aefa2b
MinhajSadik <MinhajoSadik@gmail.com>
2022-02-23 23:44:30 +0600

    [ADD] new class and prototype concept which was uncleared

--- JavaScripts/Advanced_Concept/classes.js
+++ JavaScripts/Advanced_Concept/classes.js
@@ -15,3 +15,45 @@ Persons.prototype = {
 const sadik = new Persons("sadik", 24);
 sadik.eat();
 sadik.getName();
+
+// prototype chaining in javascript
+
+const f = function Perosn() {};
+Object.prototype.mina = function () {
+  console.log("minaj");
+};
+
+const p = {};
+p.mina();
+
+// class and prototype use case in javascript
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Professions(name, age, type, details) {
+  Person.call(this);
+  this.name = name;
+  this.age = age;
+  this.type = type;
+  this.details = details;
+}
+
+Person.prototype = {
+  eat: function () {
+    console.log(`${this.name} is eating`);
+  },
+};
+
+Professions.prototype = Object.create(Person.prototype);
+Professions.prototype.constructor = Professions;
+
+let minhaj = new Professions(
+  "minhaj",
+  24,
+  "student, cricketer, developer",
+  "all-rounder"
+);
+
+console.log(minhaj);
