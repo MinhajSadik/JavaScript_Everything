62b68e850f236eb70c634aeac2300a9fda84c74c
MinhajSadik <MinhajoSadik@gmail.com>
2022-02-24 13:18:57 +0600

    [ADD] numerousworking i have done, class, prototype, getter method, setter method, static method, inheritance, polymorphism etc

--- JavaScripts/Advanced_Concept/classes.js
+++ JavaScripts/Advanced_Concept/classes.js
@@ -1,59 +1,103 @@
-function Persons(name, age) {
-  this.name = name;
-  this.age = age;
-  this.getName = function () {
+/**
+ * class example in javascript
+ * @class
+ * @constructor
+ * inherit from person and inheritance
+ */
+
+// class example with inheritance in javascript
+class Person {
+  // parent class
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    console.log(`${this.name} is eating`);
+  }
+  play() {
+    console.log(`${this.name} is playing`);
+  }
+}
+
+class Profession extends Person {
+  //sub class
+  constructor(name, age, type, details) {
+    super(name, age);
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    this.details = details;
+  }
+  play() {
+    super.play();
+    console.log(`${this.name} is playing cricket`);
+  }
+}
+
+const sadik = new Profession("Sadik", 25, "Student", "CSE");
+sadik.play();
+
+const minhaj = new Person("Minhaj", 25);
+minhaj.eat();
+
+// class example with getter and setter
+class Person1 {
+  constructor(name, age) {
+    // property
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    // method
+    console.log(`${this.name} is eating`);
+  }
+  get setName() {
+    //getter is nothing, just a method, it behaves like a method but we'll access it like a property
     return this.name;
-  };
+  }
+  //setter takes a parameter and sets the value of the property to the parameter, then outside of the class we'll set/change the value of the property
+  set setName(name) {
+    this.name = name;
+  }
+
+  static isEqualStatic(person1, person2) {
+    // the static method is a method that is not associated with any instance of the class but rather with the main class itself.
+    return person1.age === person2.age;
+  }
 }
 
-Persons.prototype = {
-  eat: function () {
-    console.log(`${this.name} is eating`);
-  },
-};
+const sadik1 = new Person1("MINHAJ", 25);
+const minhaj1 = new Person1("SADIK", 25);
 
-const sadik = new Persons("sadik", 24);
-sadik.eat();
-sadik.getName();
+sadik1.setName = "SHARMINA";
 
-// prototype chaining in javascript
+console.log(Person1.isEqualStatic(sadik1, minhaj1));
 
-const f = function Perosn() {};
-Object.prototype.mina = function () {
-  console.log("minaj");
-};
+console.log(sadik1.name);
 
-const p = {};
-p.mina();
-
-// class and prototype use case in javascript
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+// class example with polymorphism
+class Programmers {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  play() {
+    console.log(`${this.name} is playing`);
+  }
 }
 
-function Professions(name, age, type, details) {
-  Person.call(this);
-  this.name = name;
-  this.age = age;
-  this.type = type;
-  this.details = details;
+class Coders extends Programmers {
+  constructor(name, age, type, info) {
+    super(name, age);
+    this.type = type;
+    this.info = info;
+  }
+  play() {
+    super.play();
+    console.log(`${this.name} is playing with code`);
+  }
 }
 
-Person.prototype = {
-  eat: function () {
-    console.log(`${this.name} is eating`);
-  },
-};
-
-Professions.prototype = Object.create(Person.prototype);
-Professions.prototype.constructor = Professions;
-
-let minhaj = new Professions(
-  "minhaj",
-  24,
-  "student, cricketer, developer",
-  "all-rounder"
-);
-
-console.log(minhaj);
+const minhajs = new Coders("minhaj", 25, "coding", "javascript");
+minhajs.play();
--- JavaScripts/Advanced_Concept/prototype.js
+++ JavaScripts/Advanced_Concept/prototype.js
@@ -141,3 +141,64 @@ class PersonClass {
 const minhajClass = new PersonClass("minhaj", 24);
 minhajClass.eat();
 const sadikClass = new PersonClass("sadik", 24);
+
+// move forward on the prototype to class
+function Persons(name, age) {
+  this.name = name;
+  this.age = age;
+  this.getName = function () {
+    return this.name;
+  };
+}
+
+Persons.prototype = {
+  eat: function () {
+    console.log(`${this.name} is eating`);
+  },
+};
+
+const sadik = new Persons("sadik", 24);
+sadik.eat();
+sadik.getName();
+
+// prototype chaining in javascript
+
+const f = function Perosn() {};
+Object.prototype.mina = function () {
+  console.log("minaj");
+};
+
+const p = {};
+p.mina();
+
+// class and prototype use case in javascript
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Professions(name, age, type, details) {
+  Person.call(this); // overriding the constructor
+  this.name = name;
+  this.age = age;
+  this.type = type;
+  this.details = details;
+}
+
+Person.prototype = {
+  eat: function () {
+    console.log(`${this.name} is eating`);
+  },
+};
+
+Professions.prototype = Object.create(Person.prototype);
+Professions.prototype.constructor = Professions;
+
+let minhaj = new Professions(
+  "minhaj",
+  24,
+  "student, cricketer, developer",
+  "all-rounder"
+);
+
+console.log(minhaj);
