c46aa8c9cd7ed6439c061f2e6aa1612921dd951d
MinhajSadik <MinhajoSadik@gmail.com>
2022-02-24 13:23:27 +0600

    [ADD] i have created this.js file and have done basic setup

---
+++ JavaScripts/Advanced_Concept/this.js
@@ -0,0 +1,14 @@
+/**
+ * this
+ * /@
+ * #implicit binding
+ * #explicit binding
+ * #new binding
+ * #window binding
+ * @/
+ * call
+ * bind
+ * apply
+ */
+
+// started 'this' with implicit binding
