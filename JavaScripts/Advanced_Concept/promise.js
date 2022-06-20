6129f883b8a4d6e4707583979466e1a760bd7da0
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-20 19:36:14 +0600

    [ADD] javascript promise async await learing

---
+++ JavaScripts/Advanced_Concept/promise.js
@@ -0,0 +1,29 @@
+/**
+ * promises are used to handle asynchronous operations
+ * async await is used to handle asynchronous operations
+ *
+ */
+
+// 1. Promises are used to handle asynchronous operations
+
+const promise = new Promise((resolve, reject) => {
+  const hasMeeting = true;
+  if (hasMeeting) {
+    const meeting = {
+      title: "Meeting",
+      date: "12/12/2019",
+    };
+    resolve(meeting);
+  } else {
+    const error = new Error("No meeting found");
+    reject(error);
+  }
+});
+
+promise
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
