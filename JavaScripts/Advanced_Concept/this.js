7dbc7b584a3b2674b9e6ced81943c3d9eacefd53
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-22 01:27:55 +0600

    [ADD] call, apply, bind coding

--- JavaScripts/Advanced_Concept/this.js
+++ JavaScripts/Advanced_Concept/this.js
@@ -15,4 +15,24 @@
 function implicit() {
   console.log(this);
 }
-implicit();
+// implicit();
+
+const printName = function () {
+  console.log(`${this.name} is ${v1}, ${v2} & ${v3}`);
+};
+
+const minhaj = {
+  name: "minhaj",
+  age: 24,
+};
+
+const v1 = "programmer";
+const v2 = "cricketer";
+const v3 = "lover";
+
+const v = [v1, v2, v3];
+
+printName.call(minhaj, v1, v2, v3);
+printName.apply(minhaj, v);
+printName.bind(minhaj, v1, v2, v3);
+nameInstace();
