f81587c68c544c501f8b01223d944b4d67f4d320
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-22 14:59:03 +0600

    [ADD] this new binding and window bing learnt

--- JavaScripts/Advanced_Concept/this.js
+++ JavaScripts/Advanced_Concept/this.js
@@ -39,5 +39,27 @@ const v = [v1, v2, v3];
 
 printName.call(minhaj, v1, v2, v3);
 printName.apply(minhaj, v);
-printName.bind(minhaj, v1, v2, v3);
-// nameInstace();
+const nameInstace = printName.bind(minhaj, v1, v2, v3);
+nameInstace();
+
+//new binding
+function Person(name, age) {
+  // const this = Object.create({});
+  this.name = name;
+  this.age = age;
+  console.log(`${this.name} is ${this.age}`);
+  // return this
+}
+
+const sakib = new Person("minhaj", 24);
+
+// window binding
+function PrintName(name) {
+  console.log(`${this.name}`); //undefined
+}
+
+const window = {
+  name: "window",
+};
+
+PrintName();
