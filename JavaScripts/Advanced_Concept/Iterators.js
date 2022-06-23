182d8ffbb5e0cb4e7bc6b1326115093aec7cc7f8
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-23 18:11:06 +0600

    [ADD] iterators learnt advanced concepts

--- JavaScripts/Advanced_Concept/Iterators.js
+++ JavaScripts/Advanced_Concept/Iterators.js
@@ -11,8 +11,24 @@ const iterator = array[Symbol.iterator]();
 console.log(iterator.next());
 console.log(iterator.next());
 
-//Custom Iterator
+//String Iterator Overrides
 String.prototype[Symbol.iterator] = function () {
+  let index = this.length;
+  return {
+    next: function () {
+      if (index === 0) {
+        return { done: true };
+      }
+      index--;
+      return { value: "ok", done: false };
+    },
+  };
+};
+
+console.log([..."minhaj"]);
+
+// Array Iterator Overrides
+Array.prototype[Symbol.iterator] = function () {
   let index = this.length;
   return {
     next: function () {
@@ -24,5 +40,61 @@ String.prototype[Symbol.iterator] = function () {
     },
   };
 };
+console.log(array);
 
-console.log([..."minhaj"]);
+// Map Iterator Overrides
+Map.prototype[Symbol.iterator] = function () {
+  let index = this.size;
+  return {
+    next: function () {
+      if (index === 0) {
+        return { done: true };
+      }
+      index--;
+      return { value: this.entries(), done: false };
+    },
+  };
+};
+console.log([
+  [1, "minhaj"],
+  [2, "sharmina"],
+  [3, "sharminhaj"],
+]);
+
+//Custom Iterator
+function* customIterator() {
+  yield array[0];
+  yield array[1];
+  yield array[2];
+}
+const cIterator = customIterator();
+console.log(cIterator.next());
+console.log(cIterator.next());
+console.log(cIterator.next());
+console.log(cIterator.next());
+
+//Custom Iterator same as iterator
+function range(start, end, step) {
+  let current = start;
+  return {
+    [Symbol.iterator]: function () {
+      return {
+        next() {
+          let result;
+          if (current <= end) {
+            result = { value: current, done: false };
+            current += step;
+            return result;
+          }
+          return { value: undefined, done: true };
+        },
+      };
+    },
+  };
+  //   return Array.from(
+  //     { length: (end - start) / step },
+  //     (_, i) => start + i * step
+  //   );
+}
+
+console.log([...range(100, 10000, 5)]);
