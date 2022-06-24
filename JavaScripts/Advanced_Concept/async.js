f565ad53ddf22d91af22ef769027abe59ab50314
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-25 00:23:03 +0600

    [ADD] async and generator usecase learnt

--- JavaScripts/Advanced_Concept/async.js
+++ JavaScripts/Advanced_Concept/async.js
@@ -1,23 +1,88 @@
-const takeOrder = (customer, callBack) => {
-  console.log(`Order for ${customer} received`);
-  callBack(customer);
-};
+//sync code
+// const takeOrder = (customer, callBack) => {
+//   console.log(`Order for ${customer} received`);
+//   callBack(customer);
+// };
 
-const processOrder = (customer, callback) => {
-  console.log(`Order for ${customer} is processing`);
-  setTimeout(() => {
-    console.log(`Order for ${customer} is ready`);
-    console.log(`Order for ${customer} is delivered`);
-    callback(customer);
-  }, 3000);
-};
+// const processOrder = (customer, callback) => {
+//   console.log(`Order for ${customer} is processing`);
+//   setTimeout(() => {
+//     console.log(`Order for ${customer} is ready`);
+//     console.log(`Order for ${customer} is delivered`);
+//     callback(customer);
+//   }, 3000);
+// };
 
-const completeOrder = (customer) => {
-  console.log(`Order for ${customer} is completed`);
-};
+// const completeOrder = (customer) => {
+//   console.log(`Order for ${customer} is completed`);
+// };
 
-takeOrder("Minhaj", (customer) => {
-  processOrder(customer, (customer) => {
-    completeOrder(customer);
-  });
+// takeOrder("Minhaj", (customer) => {
+//   processOrder(customer, (customer) => {
+//     completeOrder(customer);
+//   });
+// });
+
+//async code
+const hasMeeting = false;
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingDetails = {
+      name: "Technical Meeting",
+      date: new Date().toLocaleDateString(),
+      location: "Google Meet",
+    };
+    resolve(meetingDetails);
+  } else {
+    const error = new Error("Meeting is already scheduled");
+    reject(error);
+  }
 });
+
+const addToCalendar = (meetingDetails) => {
+  const celendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.date} at ${meetingDetails.location}`;
+  return Promise.resolve(celendar);
+};
+
+// meeting
+//   .then(addToCalendar)
+//   .then((res) => {
+//     //resolve data
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     //reject data
+//     console.error(error.message);
+//   });
+
+const promise = Promise.resolve("Promise 0 resolved");
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise 1 resolved");
+  }, 1000);
+});
+
+// Promise.all([promise, promise1]).then((res) => {
+//   console.log(res);
+// });
+
+//async-await
+const asyncFunc = async () => {
+  return `Asynchronous`;
+};
+
+// console.log(await asyncFunc());
+
+//proper usecase of async-await
+async function myMeeting() {
+  try {
+    const meetingDetails = await meeting;
+    const calendar = await addToCalendar(meetingDetails);
+    console.log(calendar);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+myMeeting();
+console.log("End of program");
--- JavaScripts/Advanced_Concept/generators.js
+++ JavaScripts/Advanced_Concept/generators.js
@@ -26,3 +26,44 @@ function* generator() {
 
 const iterator = generator();
 console.log(iterator.next());
+
+//compare with custom Iterator to generator
+const object = {
+  value1: 1,
+  value2: 2,
+  value3: 3,
+  value4: 4,
+  value5: 5,
+};
+Object.prototype[Symbol.iterator] = function () {
+  const entries = Object.entries(this);
+  let count = entries.length;
+  let index = 0;
+  return {
+    next() {
+      if (count > 0) {
+        let result = { done: false, value: entries[index][1] };
+        count--;
+        index++;
+        return result;
+      }
+      return { done: true };
+    },
+  };
+};
+
+//generator function
+function* objGenerator(obj) {
+  const entries = Object.entries(obj);
+  for (let entry of entries) {
+    yield entry[1];
+  }
+}
+
+const objIterator = objGenerator(object);
+
+// for (let value of object) {
+for (let value of objIterator) {
+  console.log(value);
+}
+// console.log([...object]);
