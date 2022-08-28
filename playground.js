07ed183518453cd26d6f454314127f6bb7a319be
MinhajSadik <MinhajoSadik@gmail.com>
2022-08-28 15:26:38 +0600

    [DONE] test-cases

--- playground.js
+++ playground.js
@@ -66,14 +66,7 @@
 //   )
 // );
 
-//graphql book query
-// Using GraphQL, write a query to return a book with an argument of ISBN which is equal to the string 0743273567. A book is retrieved from the database and converted into an object with the following structure:
-
-//For the book query, return the fields in the following order:
-// title, author, published, publisher, pages, genres, reviews;
-
-// A review object has the fields: author, date, and content. Finally, make sure to set a limit of 1 for the reviews field.
-
+//graphQl book query
 // query{
 //   book(isbn: "0743273567"){
 //     title,
@@ -89,30 +82,6 @@
 //     }
 //   }
 // }
-
-// Back-end Challenge
-// In the JavaScript file, write a program to perform a GET request on the route https:
-//coderbyte.com/api/challenges/json/json-cleaning and then clean the object according to the following rules:
-
-// Remove all keys that have values of N/A, -, or empty strings. If one of these values appear in an array, remove that single item from the array. Then console log the modified object as a string.
-
-// Example Input
-//  {
-//   "name": { "first": "Robert", "middle": "", "last": "Smith" },
-//   "age": 25,
-//   "DOB": "-",
-//   "hobbies": ["running", "coding", "-"],
-//   "education": { "highschool": "N/A", "college": "Yale" }
-// }
-
-// Example Output
-// //  {
-//   "name": { "first": "Robert", "last": "Smith" },
-//   "age": 25,
-//   "hobbies": ["running", "coding"],
-//   "education": { "college": "Yale" }
-// }
-// {"name":{"first":"Robert","last":"Smith"},"age":25,"hobbies":["running","coding"],"education":{"college":"Yale"}}
 
 import https from "https";
 https
@@ -156,3 +125,125 @@ https
   .on("error", (err) => {
     console.log("Error: " + err.message);
   });
+
+// Check if you are all set. For this problem you just need to print "All Set!" and you're ready to go!
+// Input Format
+
+function allSet() {
+  console.log("All Set!");
+}
+allSet();
+
+// Problem Statement
+// Rahim was decorating the book shelf with his books. He has two bookshelves, and he wants to put equal books in each book shelf. At first he put some random number of books on both shelves by guessing numbers. Now he wants to know the difference between the number of books in these two shelves so that he can rearrange them. Help him!
+
+// You will be given two integer numbers A and B , you need to print the difference between these two numbers. Remember, difference is always positive.
+
+// Input Format
+
+// Input will contain two integers A and B.
+
+// Constraints
+
+// A (0 <= A <= 10^18)
+// B (0 <= B <= 10^18)
+// Output Format
+
+// Output a single line, the difference between A and B.
+
+function bookShelf(A, B) {
+  const result = A - B;
+  if (result < 0) {
+    return -result;
+  } else {
+    return result;
+  }
+}
+console.log(bookShelf(10, 50));
+
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if the object has a property with key 'b'
+// Return false otherwise
+// Tipp: test case 3 is a bit tricky because the value of property 'z' is undefined
+// But the property itself exists
+function hasProperty(a, b) {
+  // return a.hasOwnProperty(b);
+  return b in a;
+}
+console.log(hasProperty({ a: 1, b: 2 }, "a"));
+
+// Write a function that takes a string as argument
+// Create an object that has a property with key 'key' and a value equal to the string
+// Return the object
+
+function createObject(str) {
+  return { key: str };
+}
+console.log(createObject("hello"));
+
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function nthChar(a, n) {
+  return a[n];
+}
+console.log(nthChar("hello", 1));
+
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+function firstThree(a) {
+  return a.substring(0, 3);
+}
+console.log(firstThree("hello"));
+
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+function lastThree(a) {
+  return a.substring(a.length - 3);
+}
+
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+// Return the resulting array
+function firstThreeElements(a) {
+  return a.slice(0, 3);
+}
+console.log(firstThreeElements([1, 2, 3, 4, 5]));
+
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+function nthElement(a, n) {
+  return a[n];
+}
+console.log(nthElement([1, 2, 3, 4, 5], 3));
+
+// Write a function that takes an array (a) as argument
+// Extract the last 3 elements of 'a'
+// Return the resulting array
+function lastThreeElements(a) {
+  return a.slice(-3);
+}
+console.log(lastThreeElements([1, 2, 3, 4, 5]));
+
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+function getProperty(obj, str) {
+  return obj[str];
+}
+console.log(getProperty({ a: 1, b: 2 }, "a"));
+
+// Write a function that takes an array (a) as argument
+// Remove the first 3 elements of 'a'
+// Return the result
+function removeFirstThreeElements(a) {
+  return a.slice(3);
+}
+console.log(removeFirstThreeElements([1, 2, 3, 4, 5]));
+
+//expected output
+(function () {
+  var a = (b = 5);
+})();
+
+console.log(b);
