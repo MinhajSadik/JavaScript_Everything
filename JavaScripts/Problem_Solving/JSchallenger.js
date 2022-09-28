c0b11717271c57f7f90c0cc2a24b6d99f889603a
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-29 01:10:07 +0600

    [LEARNED] some of JavaScript challeges and solved it

--- JavaScripts/Problem_Solving/JSchallenger.js
+++ JavaScripts/Problem_Solving/JSchallenger.js
@@ -9,70 +9,91 @@ function merge_options(x, y) {
   return { ...x, ...rest, d: b };
 }
 
-function myFunction(x, y) {
+function mergeOption(x, y) {
   y["d"] = y["b"];
   delete y["b"];
 
   return { ...x, ...y };
 }
 
-console.log(merge_options({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }));
+// console.log(merge_options({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }));
 
-// write a function that takes an object as an argument return true if the object has a property with key b
-// const object1 = {
-//   prop: "exists",
-// };
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if the object has a property with key 'b', but only if it has a truthy value
+// In other words, it should not be null or undefined or false
+// Return false otherwise
+function property_checker(a, b) {
+  return Boolean(a[b]);
+}
 
-// console.log(Object.hasOwn(object1, "prop"));
-// // expected output: true
+function propertyChecker(a, b) {
+  return a[b] ? true : false;
+}
 
-// var obj = {
-//   a: undefined,
-//   b: null,
-//   c: false,
-// };
+// console.log(propertyChecker({ a: 1, b: 2, c: 3 }, "b"));
+// console.log(property_checker({ a: 1, b: 2, c: 3 }, "b"));
 
-// // a, b, c all found
-// for (var prop in obj) {
-//   document.writeln("Object1: " + prop);
-// }
+// Write a function that takes two date instances (a and b) as arguments
+// It should return true if a is earlier than b
+// It should return false otherwise
+function checkEarlier(a, b) {
+  return a < b;
+}
 
-// function Class() {
-//   this.a = undefined;
-//   this.b = null;
-//   this.c = false;
-// }
+function check_earlier(a, b) {
+  return a < b;
+}
 
-// Class.prototype = {
-//   a: undefined,
-//   b: true,
-//   c: true,
-//   d: true,
-//   e: true,
-// };
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+function percentage(a, b) {
+  return (a * b) / 100;
+}
 
-// var obj2 = new Class();
+function percentage1(a, b) {
+  return (b / 100) * a;
+}
 
-// // a, b, c, d, e found
-// for (var prop in obj2) {
-//   document.writeln("Object2: " + prop);
-// }
+console.log(percentage(10, 1));
 
-// function hasOwnProperty(obj, prop) {
-//   var proto = obj.__proto__ || obj.constructor.prototype;
-//   return prop in obj && (!(prop in proto) || proto[prop] !== obj[prop]);
-// }
+// Write a function that takes a Set as argument
+// Convert the Set to an Array
+// Return the Array
+function convertSetToArray(set) {
+  return Array.from(set);
+}
 
-// if (Object.prototype.hasOwnProperty) {
-//   var hasOwnProperty = function (obj, prop) {
-//     return obj.hasOwnProperty(prop);
-//   };
-// }
+function convert_set_to_Array(set) {
+  return [...set];
+}
 
-// // a, b, c found in modern browsers
-// // b, c found in Safari 2.0.1 and older
-// for (var prop in obj2) {
-//   if (hasOwnProperty(obj2, prop)) {
-//     document.writeln("Object2 w/ hasOwn: " + prop);
-//   }
-// }
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+function extract_first_half_string(a) {
+  return a.slice(0, a.length / 2);
+}
+
+function extractFirstHalfString(a) {
+  return a.slice(0, a.length / 2);
+}
+
+// Write a function that takes an object as argument
+// In some cases the object contains other objects with some deeply nested properties
+// Return the property 'b' of object 'a' inside the original object if it exists
+// If not, return undefined
+function nastedObjectProperty(obj) {
+  const {
+    a: { b },
+  } = obj;
+
+  return b !== undefined ? b : null;
+}
+
+function nasted_object_property(obj) {
+  return null;
+}
+
+console.log(nastedObjectProperty({ a: { b: { c: 2 } } }));
+
+// how to return nested object property?
