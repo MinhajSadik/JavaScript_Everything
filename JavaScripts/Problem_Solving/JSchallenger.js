6eb51fb62597f11927752fccaf76ca10bacc4d9a
MinhajSadik <MinhajoSadik@gmail.com>
2022-12-18 07:01:56 +0600

    [DONE] one problem soving done

--- JavaScripts/Problem_Solving/JSchallenger.js
+++ JavaScripts/Problem_Solving/JSchallenger.js
@@ -152,3 +152,15 @@ function split_digit(a) {
 }
 
 console.log(splitDigit(100));
+
+
+// Round a number to 2 decimal places
+// Write a function that takes a number(a) as argument.Round a to the 2nd digit after the comma.Return the rounded number
+function roundNumber(a) {
+  return Math.round(a * 100) / 100;
+}
+// function round_number(a) {
+//   return Number(a.toFixed(2));
+// }
+
+
